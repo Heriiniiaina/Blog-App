357ef4fcd2670adbe3379d2ace08c831dc4503cd
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-15 19:04:25 +0100

    create new user

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { ErrorHandler } from "../middlewares/errorHandler.js"
 import { registerSchema } from "../middlewares/validator.js"
+import { createNewUser } from "../services/auth.service.js"
 
 export const register = async(req,res,next)=>{
     const {nom,prenom,email,password} = req.body
@@ -9,7 +10,12 @@ export const register = async(req,res,next)=>{
         const {error} = registerSchema.validate({nom,prenom,email,password})
         if(error)
             return next(new ErrorHandler(error.details[0].message,400))
+        const user= await createNewUser({nom,prenom,email,password})
+        res.status(201).json({
+            success:true,
+            message:"Compte créé avec succées"
+        })
     } catch (error) {
-        
+        next(new ErrorHandler(error.message))
     }
 }
--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -1,3 +1,18 @@
-export const createNewUser = async(user)=>{
+import { User } from "../models/user.model.js"
+import { hashPassword } from "../utils/bcrypt.config.js"
 
+export const createNewUser = async(userData)=>{
+    try {
+        const passowrd = await hashPassword(userData.passowrd)
+        const user = new User({
+            nom:userData.nom,
+            prenom:userData.prenom,
+            email:userData.email,
+            passowrd:passowrd
+        })
+        await user.save()
+        return user
+    } catch (error) {
+        throw new Error(error.message)
+    }
 }
