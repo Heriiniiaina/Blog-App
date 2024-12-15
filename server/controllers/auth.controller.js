e980e373b585ab81b93036f50df96bdb3b6cd405
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-15 18:52:14 +0100

    controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,8 +1,15 @@
 import { ErrorHandler } from "../middlewares/errorHandler.js"
+import { registerSchema } from "../middlewares/validator.js"
 
 export const register = async(req,res,next)=>{
     const {nom,prenom,email,password} = req.body
     if(!nom | !prenom | !email | !password)
         return next(new ErrorHandler("Veuillez remplir le formulaire",400))
-    
+    try {
+        const {error} = registerSchema.validate({nom,prenom,email,password})
+        if(error)
+            return next(new ErrorHandler(error.details[0].message,400))
+    } catch (error) {
+        
+    }
 }
