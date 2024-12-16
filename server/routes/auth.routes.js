10d1d91ab92c8f8e1577d8e82086008c16330fbb
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-16 06:55:52 +0100

    login

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from "express"
-import { register } from "../controllers/auth.controller.js"
+import { login, register } from "../controllers/auth.controller.js"
 const router = express.Router()
 
 router.post("/signup",register)
+router.post("/sign",login)
 export default router
