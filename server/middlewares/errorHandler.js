0d69f54982fee08c161b165df87e78305bff18d4
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-16 06:52:46 +0100

    error

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -11,7 +11,8 @@ export const errorMiddleware = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500
     res.status(err.statusCode).json({
         success:false,
-        message:err.message
+        message:err.message,
+      
 
     })
 }
