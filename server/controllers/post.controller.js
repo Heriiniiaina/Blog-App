fa84f0ac01cf1cf5239b058c19a3ac092f0fa905
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-16 11:19:31 +0100

    fix

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,5 +1,7 @@
 import { ErrorHandler } from "../middlewares/errorHandler.js"
+import { postSchema } from "../middlewares/validator.js"
 import { getUserById } from "../services/auth.service.js"
+import { createNewPost } from "../services/post.service.js"
 
 export const addNewPost = async(req,res,next)=>{
     const {content,userId} = req.body
@@ -7,8 +9,17 @@ export const addNewPost = async(req,res,next)=>{
         return next(new ErrorHandler("Veuillez repmlir le champ",400))
     try {
         const user = await getUserById(userId)
-
+        const {error} = postSchema.validate({content})
+        if(error)
+            throw new ErrorHandler(error.details[0].message)
+        const post = await createNewPost({content,userId:user._id})
+        res.status(201).json({
+            message:"Poste créé",
+            success:true,
+            post
+        })
     } catch (error) {
-        next(new ErrorHandler(error.message))
+        console.log(error)
+        next(new ErrorHandler(error.message,error.statusCode))
     }
 }
