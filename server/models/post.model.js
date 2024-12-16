72ef42e63817d43dbf4ec4b2fb02a1ab879ae64d
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-16 08:48:44 +0100

    create post

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -7,6 +7,7 @@ const PostSchema = mongoose.Schema({
     },
     image:{
         type:String,
+        default:null
     },
     user:{
         type:mongoose.Schema.Types.ObjectId,
---
+++ server/services/post.service.js
@@ -0,0 +1,16 @@
+import { ErrorHandler } from "../middlewares/errorHandler.js";
+import { Post } from "../models/post.model.js";
+
+export const createNewPost = async (postData)=>{
+    try {
+        const post = new Post({
+            content:postData.content,
+            image:postData.image,
+            user:postData.userId
+        })
+        await post.save()
+    } catch (error) {
+        throw new ErrorHandler(error.message)
+    }
+}
+
