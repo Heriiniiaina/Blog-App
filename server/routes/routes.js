78c03066ee286a3549bb6edeb2b7c8316ecbdae5
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-17 08:17:49 +0100

    fix

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,10 @@
 import express from "express"
 import authRoute from "./auth.routes.js"
 import postRoute from "./post.routes.js"
+import profileRoute from "./user.routes.js" 
 const router = express.Router()
 const BASE_URL = "/blog"
 router.use(`${BASE_URL}/auth`,authRoute)
 router.use(`${BASE_URL}/post`,postRoute)
+router.use(`${BASE_URL}/profile`,profileRoute)
 export default router
