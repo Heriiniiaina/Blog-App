da452c9a5075cdda89491ab44eba6006db03606a
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-17 08:17:11 +0100

    cloudinary

--- server/utils/cloudinary.config.js
+++ server/utils/cloudinary.config.js
@@ -1,7 +1,8 @@
 import {v2 as cloudinary} from "cloudinary"
-
+import { config } from "dotenv"
+config()
 cloudinary.config({
-    cloud_name:process.env.cloudinary_cloud_name,
+    cloud_name:process.env.name,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET
     
