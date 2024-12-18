794fdad2283ca4b6a634193321b5ad636b9b7e11
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-18 08:06:07 +0100

    login page

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import InputCustom from "../components/InputCutom/InputCustom"
+import ButtonCustom from "../components/ButonCustom/ButtonCustom"
 
 
 
@@ -7,12 +8,21 @@ import InputCustom from "../components/InputCutom/InputCustom"
 const Login = () => {
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
-  return (
-    <div>
+    const handleSubmit = async (e:React.FormEvent)=>{
+        e.preventDefault()
+        console.log(email+"  " + password)
+    }
+    return (
+    <div className="flex flex-col justify-center items-center gap-4 h-screen">
         <h1>Bon retour</h1>
-        <form action="">
-            <InputCustom type="text" placeHolder="Votre email" onChange={(e)=> setEmail(e.target.value)} />
-            <InputCustom type="password"  placeHolder="Votre mot de passe" onChange={(e)=>setPassword(e.target.value)}/>
+        <form action="" onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-3">
+            <InputCustom type="text" placeHolder="Votre email" onChange={(e)=> setEmail(e.target.value)} 
+                className="border border-black pl-2 p-1 rounded-xl outline-none"
+            />
+            <InputCustom type="password"  placeHolder="Votre mot de passe" onChange={(e)=>setPassword(e.target.value)}
+                className="border border-black pl-2 p-1 rounded-xl outline-none"
+            />
+            <ButtonCustom className="bg-blue-500 w-[200px] p-3 rounded text-cyan-50" type="submit">Se Connecter</ButtonCustom>
         </form>
     </div>
   )
