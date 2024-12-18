45477453fe9bdfe48d574244de940b8a49d26fdc
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-18 16:33:52 +0100

    send forgot password code

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -115,3 +115,57 @@ export const logout = async(req,res,next)=>{
     }
    
 }
+export const sendForgotPasswordCode = async(req,res,next)=>{
+    const {email} = req.body
+    if(!email)
+        return next(new ErrorHandler("Veuillez entrer votre email",400))
+    try {
+        const user = getAllUserInfo(email)
+        if(!user)
+            next(new ErrorHandler("Auccun utilisateur avec cette email",404))
+        const code = Math.floor(Math.random() * 1000000).toString()
+        const hashedCode = hashCode(code,process.env.HMAC_KEY)
+        const info = transport.sendMail({
+            from:process.env.SENDING_EMAIL_ADRESS,
+            to:user.email,
+            subject:"Reset password code",
+            html:"<h1>" + code + "</h1>"
+        })
+        if((await info).accepted==user.email){
+            user.forgotPasswordCode = hashedCode
+            user.forgotPasswordCodeValidity = Date.now()
+            user.save()
+            return res.status(200).json({
+                success:true,
+                message:"code envoyer a votre email"
+            })
+        }
+        throw new ErrorHandler("Quelque chose s'est mal passé",500)
+    } catch (error) {
+        next(new ErrorHandler(error.message,error.statusCode))
+    }
+}
+export const verifyForgotPasswordCode = async(req,res,next)=>{
+    const {email,code} = req.body
+    if(!email || !code)
+        return next(new ErrorHandler("Veuillez remplir les informations",400))
+    try {
+        const user = await getAllUserInfo(email)
+        if(!user) 
+            throw new ErrorHandler("Auccun utilisateur avec cette email",404)
+        if(Date.now() - user.forgotPasswordCodeValidity > 5 * 3600000)
+            throw new ErrorHandler("Code expiré!",400)
+        const hashedCode = hashCode(code,process.env.HMAC_KEY)
+        if(hashedCode !== user.forgotPasswordCode)
+            throw new ErrorHandler("Code incorrect",400)
+        user.forgotPasswordCode = undefined
+        user.forgotPasswordCodeValidity = undefined
+        user.save()
+        res.status(200).json({
+            success:true,
+            message:"Code correct"
+        })
+    } catch (error) {
+        next(new ErrorHandler(error.message,error.statusCode))
+    }
+}
