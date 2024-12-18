64987c6f1c038fa78262fbb7180a8e6373b8a690
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-18 07:18:20 +0100

    Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
 
-type Props = {}
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Login from "./Pages/Login"
+import Register from "./Pages/Register"
+import Home from "./Pages/Home"
 
-const App = (props: Props) => {
+
+const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login/>} />
+            <Route path="/signup" element={<Register/>} />
+            <Route path="/home" element={<Home/>} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
