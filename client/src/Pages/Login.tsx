357bf8b5abcad90936223fec312904ba9c6bac92
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-18 07:42:42 +0100

    login

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -1,10 +1,20 @@
+import React, { useState } from "react"
+import InputCustom from "../components/InputCutom/InputCustom"
 
 
 
 
 const Login = () => {
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
   return (
-    <div>Login</div>
+    <div>
+        <h1>Bon retour</h1>
+        <form action="">
+            <InputCustom type="text" placeHolder="Votre email" onChange={(e)=> setEmail(e.target.value)} />
+            <InputCustom type="password"  placeHolder="Votre mot de passe" onChange={(e)=>setPassword(e.target.value)}/>
+        </form>
+    </div>
   )
 }
 
