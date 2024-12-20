10cbfdd96d35a7191003bc19198fbe8f55f9c608
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-20 18:39:41 +0100

    authState

--- client/src/store/slices/auth.slice.ts
+++ client/src/store/slices/auth.slice.ts
@@ -1,1 +1,31 @@
 import {createSlice} from "@reduxjs/toolkit"
+
+interface User{
+    id:string,
+    nom:string,
+    prenom:string,
+    email:string,
+    verified:boolean
+}
+
+interface AuthState {
+    user:User | null,
+    token:string | null
+    isAuthenticiate:boolean
+}
+
+const initialState:AuthState={
+    user:null,
+    token:null,
+    isAuthenticiate:false
+}
+
+const authSlice = createSlice({
+    name:"auth-slice",
+    initialState,
+    reducers:{
+        login:()=>{
+            
+        }
+    }
+}) 
