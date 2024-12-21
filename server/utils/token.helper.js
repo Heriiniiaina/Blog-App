80ee5e93ad673facf2bc80d7661ab8a2a99fe746
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-21 18:50:18 +0100

    reglage token

--- server/utils/token.helper.js
+++ server/utils/token.helper.js
@@ -7,7 +7,8 @@ export const generateToken =(user)=>{
         userId:user._id,
         nom:user.nom,
         prenom:user.prenom,
-        email:user.email
+        email:user.email,
+        verified:user.verified
     }
     const token =  jwt.sign(userData,process.env.TOKEN_SECRET,{expiresIn:"8h"})
     return token
