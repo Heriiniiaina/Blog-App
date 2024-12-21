74a61f25826af9cf1cec8c86b1f30b975704ecbb
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-21 19:34:56 +0100

    fix route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,16 @@ import Register from "./Pages/Register"
 import Home from "./Pages/Home"
 import "./style.css"
 import Login from "./Pages/Login"
+import PublicRoute from "./components/ProtectedRoute/PublicRoute"
 
 const App = () => {
   return (
     <BrowserRouter>
         <Routes>
+          <Route element={<PublicRoute/>}>
             <Route path="/" element={<Login/>} />
+
+          </Route>
             <Route path="/signup" element={<Register />} />
             <Route path="/home" element={<Home/>} />
         </Routes>
