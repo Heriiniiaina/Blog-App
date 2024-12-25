8b8dc05510f87508419c4ec5c27c33635b0514d0
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-25 18:23:15 +0100

    act

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,11 +1,13 @@
+import Acutality from "../components/Actuality/Acutality"
 import Profile from "../components/ProfilePage/Profile"
 
 
 const Home = () => {
   return (
-   <>
+   <div>
       <Profile/>
-   </>
+      <Acutality/>
+   </div>
   )
 }
 
--- client/src/components/Actuality/Acutality.tsx
+++ client/src/components/Actuality/Acutality.tsx
@@ -1,10 +1,27 @@
+import { useEffect } from "react"
+import { PostApi } from "../../api/post.api"
+import { useDispatch } from "react-redux"
+import { setPost } from "../../store/slices/post.slice"
 
 
 
 const Acutality = () => {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    const getPost = async()=>{
+        try {
+            const post = await PostApi.getAllpost()
+            dispatch(setPost(post))
+            console.log(post)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    getPost()
+  },[])
   return (
     <div>
-        
+        sas
     </div>
   )
 }
