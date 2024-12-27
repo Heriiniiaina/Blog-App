7f6947c7b3226e6a3ef42f08911880fce30ad3fb
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-27 19:39:07 +0100

    nav

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -5,6 +5,7 @@ import Logo from "../Logo/Logo"
 import { User } from "../../Constants/UserInterface"
 import { jwtDecode } from "jwt-decode"
 import { RootState } from "../../store/store"
+import { FaSearch } from "react-icons/fa"
 
 
 
@@ -13,12 +14,13 @@ const NavBar = () => {
     const token = useSelector((store:RootState)=>store.auth.token)  || ""
     const user:User = jwtDecode(token)
   return (
-    <div className="bg-blue-500 p-3 flex items-center justify-between">
+    <div className="bg-[#fff] p-3 flex items-center justify-between">
         <div>
             <Logo/>
         </div>
-        <div>
-            <InputCustom name="search" placeHolder="search" type="text"/>
+        <div className="relative">
+            <InputCustom name="search" placeHolder="search" className="p-3 md:w-[500px] rounded-md"  type="text" style={{backgroundColor:"rgba(80,181,255,.2)"}}/>
+            <FaSearch className="text-xl absolute top-1/4 right-2 text-slate-700"/>
         </div>
         <div className="flex items-center gap-2">
             <img src={user.image} alt="" className="rounded-full w-[50px] h-[50px]"/>
