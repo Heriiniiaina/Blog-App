e05fedcd60623c3c2ce7b0b0fb8d9c9e642d1f76
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-28 14:01:12 +0100

    post

---
+++ client/src/components/PostForm/PostForm.tsx
@@ -0,0 +1,19 @@
+
+
+
+
+const PostForm = () => {
+  return (
+    <div>
+        <div>
+            <h4>Create post</h4>
+        </div>
+        <hr />
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default PostForm
