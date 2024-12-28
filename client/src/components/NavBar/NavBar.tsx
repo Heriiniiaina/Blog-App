87edfe28e4ac586d7a9f5e03666abca0820d6aa0
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-28 14:00:04 +0100

    nav

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -5,7 +5,7 @@ import Logo from "../Logo/Logo"
 import { User } from "../../Constants/UserInterface"
 import { jwtDecode } from "jwt-decode"
 import { RootState } from "../../store/store"
-import { FaBell, FaEnvelope, FaHome, FaSearch, FaSpellCheck, FaUserFriends } from "react-icons/fa"
+import { FaBell, FaEnvelope,  FaSearch,  FaUserFriends } from "react-icons/fa"
 import { FaHouse } from "react-icons/fa6"
 
 
@@ -14,7 +14,7 @@ import { FaHouse } from "react-icons/fa6"
 
 const NavBar = () => {
     const token = useSelector((store:RootState)=>store.auth.token)  || ""
-    const user:User = jwtDecode(token)
+    const user = jwtDecode(token) as User
   return (
     <div className="bg-[#fff] p-3 flex items-center justify-between">
         <div>
@@ -24,7 +24,7 @@ const NavBar = () => {
             <InputCustom name="search" placeHolder="search" className="p-3 md:w-[400px] rounded-md"  type="text" style={{backgroundColor:"rgba(80,181,255,.2)"}}/>
             <FaSearch className="text-xl absolute top-1/4 right-2 text-slate-700"/>
         </div>
-        <div className="flex items-center gap-4">
+        <div className="hidden md:flex items-center gap-4">
             <FaHouse  className="text-xl"/>
             <FaUserFriends className="text-xl"/>
             <FaBell className="text-xl"/>
@@ -32,6 +32,7 @@ const NavBar = () => {
             <div className="flex items-center gap-2">
                 <img src={user.image} alt="" className="rounded-full w-[50px] h-[50px]"/>
                 <h1>{user.nom} {user.prenom}</h1>
+               
             </div>
             
         </div>
