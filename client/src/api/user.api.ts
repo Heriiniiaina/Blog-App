de9dbc636aaab5321ddca27a036ed063fc4039ff
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-30 19:32:28 +0100

    friend

--- client/src/api/user.api.ts
+++ client/src/api/user.api.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true
-const BASE_URL = "http://localhost:8000/blog/profile"
+const BASE_URL = "http://localhost:8000/blog"
 
 
 
 export class UserApi {
     static async getUser(userId:string){
-        return ( await axios.get(`${BASE_URL}/get-user/${userId}`) ).data
+        return ( await axios.get(`${BASE_URL}/profile/get-user/${userId}`) ).data
     }
-   
+    static async getFriends(userId:string){
+        return ( await axios.get(`${BASE_URL}/friend/get-friends/${userId}`) ).data
+    }
     
 }
