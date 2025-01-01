3ba1e74e8459e296163c2d556380f93abc8dd0dd
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-01-01 11:30:15 +0100

    loading

---
+++ client/src/components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+
+
+import {OrbitProgress} from "react-loading-indicators"
+
+
+const Loading = () => {
+  return( 
+    <OrbitProgress color="#3166cc" size="large" text="" textColor="" />
+  )
+}
+
+export default Loading
