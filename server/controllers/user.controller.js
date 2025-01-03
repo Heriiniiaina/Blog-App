b2f5fbda8089a5e4d1e78db8f9cfe192ffca49cf
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-01-03 18:24:12 +0100

    userId

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -7,7 +7,14 @@ export const getUserInfo = async(req,res,next)=>{
         const user = await getUserById(userId)
         res.status(200).json({
             success:true,
-            user
+            user:{
+                userId:user._id,
+                nom:user.nom,
+                prenom:user.prenom,
+                email:user.email,
+                addresse:user.addresse,
+                image:user.image
+            }
         })
     } catch (error) {
         next(new ErrorHandler(error.message,error.statusCode))
