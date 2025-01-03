dd3b9aa864e86b27ed692cb2392d8a4765769865
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-01-03 19:10:46 +0100

    api

---
+++ client/src/api/like.api.ts
@@ -0,0 +1,8 @@
+import axios from "axios"
+
+const BASE_URL = "http://localhost:8000/blog/like"
+export class LikeApi{
+    static async likePost(postId:string,userId:string){
+        return (await axios.patch(`${BASE_URL}/like-post`,{postId:postId,userId})).data
+    }
+}
