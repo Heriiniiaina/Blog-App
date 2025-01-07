1c6d8c0f2ff4d7752fb34e57c59c5e39f07fd1a9
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-01-07 20:09:14 +0100

    add bug to fix later

--- client/src/services/post.service.ts
+++ client/src/services/post.service.ts
@@ -1,9 +1,40 @@
-import { useSelector } from "react-redux"
-import { POST } from "../Constants/PostInterface"
-import { RootState } from "../store/store"
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { PostApi } from "../api/post.api";
+import { setPost } from "../store/slices/post.slice";
+import { POST } from "../Constants/PostInterface";
+import { RootState } from "../store/store";
 
+export const getPost = (id?: string): POST | null => {
+  const dispatch = useDispatch();
+  const [post, setLocalPost] = useState<POST | null>(null);
 
-export const getPost = (id?:string):POST=>{
-    const post = useSelector((store:RootState)=>store.posts.posts.find((post)=>post._id === id)) as POST
-    return post
-}
+  // Obtenir les posts du store Redux
+  const postFromStore = useSelector((store: RootState) =>
+    store.posts.posts.find((post) => post._id === id)
+  );
+
+  useEffect(() => {
+    const fetchAllPosts = async () => {
+      if (!postFromStore) {
+        try {
+          const postsFromApi = await PostApi.getAllpost();
+          dispatch(setPost(postsFromApi));
+        } catch (error) {
+          console.error("Failed to fetch posts:", error);
+        }
+      }
+    };
+
+    fetchAllPosts();
+  }, [dispatch, postFromStore]);
+
+  useEffect(() => {
+    // Mettre à jour le post localement si trouvé dans le store
+    if (postFromStore) {
+      setLocalPost(postFromStore);
+    }
+  }, [postFromStore]);
+
+  return post;
+};
