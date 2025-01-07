9de1bbb35970bd2742cd2545d7d4c49670947542
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-01-07 20:20:37 +0100

    mbola tsy vita le bug

--- client/src/services/post.service.ts
+++ client/src/services/post.service.ts
@@ -5,36 +5,6 @@ import { setPost } from "../store/slices/post.slice";
 import { POST } from "../Constants/PostInterface";
 import { RootState } from "../store/store";
 
-export const getPost = (id?: string): POST | null => {
-  const dispatch = useDispatch();
-  const [post, setLocalPost] = useState<POST | null>(null);
-
-  // Obtenir les posts du store Redux
-  const postFromStore = useSelector((store: RootState) =>
-    store.posts.posts.find((post) => post._id === id)
-  );
-
-  useEffect(() => {
-    const fetchAllPosts = async () => {
-      if (!postFromStore) {
-        try {
-          const postsFromApi = await PostApi.getAllpost();
-          dispatch(setPost(postsFromApi));
-        } catch (error) {
-          console.error("Failed to fetch posts:", error);
-        }
-      }
-    };
-
-    fetchAllPosts();
-  }, [dispatch, postFromStore]);
-
-  useEffect(() => {
-    // Mettre à jour le post localement si trouvé dans le store
-    if (postFromStore) {
-      setLocalPost(postFromStore);
-    }
-  }, [postFromStore]);
-
-  return post;
+export const getPost = (id?: string): POST  => {
+    s
 };
