5de59e32c335836f98ab7c27745e8babb00c34f4
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-01-09 19:00:37 +0100

    cover

--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -1,10 +1,14 @@
 import { useSelector } from "react-redux"
 import DisplayUserInfo from "../components/DisplayUserInfo/DisplayUserInfo"
 import NavBar from "../components/NavBar/NavBar"
+import { RootState } from "../store/store"
+import { getUser } from "../services/user.service"
+import { User } from "../Constants/UserInterface"
 
 
 const Profile = () => {
-  
+ const user = getUser() as User
+ console.log(user)
   return (
     <div className="bg-[#f8f9fa] w-full">
 
@@ -13,7 +17,7 @@ const Profile = () => {
       </div>
       <div>
          <div>
-            d
+            <DisplayUserInfo user={user}/>
          </div>
          <div>
 
