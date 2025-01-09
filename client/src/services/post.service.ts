717faa18393927c44568bb06facef5f44a94a97e
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-01-09 17:41:00 +0100

    fuck amizay

--- client/src/services/post.service.ts
+++ client/src/services/post.service.ts
@@ -3,7 +3,9 @@ import { POST } from "../Constants/PostInterface"
 import { RootState } from "../store/store"
 
 
-export const getPost = (id:string):POST=>{
+export const getPost = (id?:string):POST | null=>{
+    if(!id)
+        return null
     const post = useSelector((store:RootState)=>store.posts.posts.find((post)=>post._id === id)) as POST
     return post
 }
