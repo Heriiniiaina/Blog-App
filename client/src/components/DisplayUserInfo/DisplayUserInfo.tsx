1da4c9054dda1fe12f685189a86405393a3c9754
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-01-12 18:09:00 +0100

    display

--- client/src/components/DisplayUserInfo/DisplayUserInfo.tsx
+++ client/src/components/DisplayUserInfo/DisplayUserInfo.tsx
@@ -1,19 +1,46 @@
-import { User } from "../../Constants/UserInterface"
-import { InputFile } from "../InputFileComponent/InputFile"
+import { User } from "../../Constants/UserInterface";
+import { InputFile } from "../InputFileComponent/InputFile";
 
-interface USERINFO{
-    user:User
+interface USERINFO {
+  user: User;
 }
-const DisplayUserInfo = ({user}:USERINFO) => {
+const DisplayUserInfo = ({ user }: USERINFO) => {
   return (
     <div>
-        <div>
-            {
-                user.cover ? <img src={user.cover} alt="" /> : <InputFile/>
-            }
+      <div>{user.cover ? <img src={user.cover} alt="" /> : <InputFile />}</div>
+      <div
+        className="z-50 absolute flex flex-col gap-2 items-center  "
+        style={{ top: "70%", left: "50%", transform: "translate(-50%, -50%)" }}
+      >
+        <img
+          src={user.image}
+          alt=""
+          className="w-[150px] h-[150px] rounded-full bg-transparent"
+        />
+        <p className="text-xl">
+          {user.nom} {user.prenom}
+        </p>
+      </div>
+      <div className="flex items-center justify-between p-2 py-4 bg-white shadow-sm shadow-slate-700">
+        <div className="flex items-center gap-2 ">
+          <div className="flex flex-col items-center">
+            <p className="text-xl text-black">Posts</p>
+            <p className="text-xl text-slate-500">50</p>
+          </div>
         </div>
+        <div className="flex items-center  gap-2">
+          <div className="flex flex-col items-center ">
+            <p className="text-xl text-black">Followers</p>
+            <p className="text-xl text-slate-500">50</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text-xl text-black">Following</p>
+            <p className="text-xl text-slate-500">50</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DisplayUserInfo
+export default DisplayUserInfo;
