93f611ce984fbee46b364e838640825432cb089e
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-01-14 18:24:43 +0100

    display comment

--- client/src/components/DisplayComment/DisplayComment.tsx
+++ client/src/components/DisplayComment/DisplayComment.tsx
@@ -1,4 +1,5 @@
 import { COMMENT } from "../../Constants/CommentInterface"
+import { getDate } from "../../services/data.service"
 
 interface CommentProps{
     comment:COMMENT
@@ -6,10 +7,14 @@ interface CommentProps{
 
 const DisplayComment = ({comment}:CommentProps) => {
   return (
-    <div className="flex flex-col gap-3 p-2 m-4">
+    <div className="flex flex-col gap-3 p-2 m-6 shadow-sm shadow-slate-400">
             <div className="flex items-center gap-2">
                 <img className="w-[40px] h-[40px] rounded-full" src={comment.user.image} alt="" />
-                <p>{comment.user.nom} {comment.user.prenom}</p>
+                <div>
+                  <p className="text-sm">{comment.user.nom} {comment.user.prenom}</p>
+                  <p className="text-[10px]">{getDate(comment.createdAt)}</p>
+                </div>
+               
             </div>
             <div>
                 <p>{comment.content}</p>
