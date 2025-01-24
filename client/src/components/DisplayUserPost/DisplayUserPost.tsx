7061c37792e07697614ddf324994e284eab69d60
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-01-24 18:31:44 +0100

    prof

--- client/src/components/DisplayUserPost/DisplayUserPost.tsx
+++ client/src/components/DisplayUserPost/DisplayUserPost.tsx
@@ -1,11 +1,32 @@
 
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { POST } from "../../Constants/PostInterface"
+import { PostApi } from "../../api/post.api"
 
 
 const DisplayUserPost = () => {
-    
     const {id} = useParams()
-    
+    const [posts,setPosts] = useState<POST[]>()
+    useEffect(()=>{
+     const fetchUserPost = async ()=>{
+     
+        if (!id) {
+          
+          return; 
+        }
+        try {
+          const res = await PostApi.getUserPost(id)
+          const post = res.posts as POST[]
+          setPosts(post)
+
+        } catch (error) {
+          
+        }
+     }
+     fetchUserPost()
+    }, [])
+    console.log(posts)
     return (
     <div> {id}</div>
   )
