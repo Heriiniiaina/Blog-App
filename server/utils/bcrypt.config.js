1bdf88b14e9abe2f3c9ed25407480ba4d9c222dc
Heriniaina <heriniainaramamonjitiana@gmail.com>
2025-02-19 18:35:01 +0100

    lele

--- server/utils/bcrypt.config.js
+++ server/utils/bcrypt.config.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt"
 
+
 export const hashPassword=async (password)=>{
     const salt = 12
     const hashedPassword = await bcrypt.hash(password,salt)
@@ -10,3 +11,4 @@ export const comparePassword = async (password,hashedPassword)=>{
     const isMatch = await bcrypt.compare(password,hashedPassword)
     return isMatch
 }
+
